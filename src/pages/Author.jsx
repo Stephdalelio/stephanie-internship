8af27013b240546c44de9d6638dedf52f0883d70
Joseph D'Alelio <joe@Josephs-MacBook-Pro.local>
2024-08-11 14:22:55 -0400

    author section

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,39 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
+import Skeleton from "./Skeleton";
+import axios from "axios";
 
-const Author = () => {
+
+export default function Author() {
+  const [skelLoad, setSkelLoad] = useState();
+  const [author, setAuthor] = useState([]);
+  const { authorId } = useParams();
+  const [follow, setFollow] = useState(false);
+
+  async function fetchData() {
+    setSkelLoad(true);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+    );
+    setSkelLoad(false);
+    setAuthor(data);
+  }
+
+  function addFollow() {
+    if (follow) {
+      setFollow(false);
+    } else {
+      setFollow(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12"></div>
 
-        <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
-
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
+            {skelLoad ? (
+              <div className="nft_skeleton">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <Skeleton 
+                      borderRadius={100} 
+                      height={150} 
+                      width={150} />
+                      <i className="fa fa-check"></i>
+                      <div className="profile_name">
+                        <h4>
+                          <Skeleton 
+                          height={24} 
+                          width={200} />
+                          <span className="profile_username">
+                            <Skeleton 
+                            height={16} 
+                            width={100} />
+                          </span>
+                          <span id="wallet" className="profile_wallet">
+                            <Skeleton 
+                            height={16} 
+                            width={250} />
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile_follow de-flex">
+                    <div className="de-flex-col">
+                      <Skeleton height={40} width={150} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_avatar">
+                      <img src={author.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {author.authorName}
+                          <span className="profile_username">
+                            @{author.tag}
+                          </span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {author.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,26 +103,39 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      {follow ? (
+                        <>
+                          <div className="profile_follower">
+                            {author.followers + 1} followers
+                          </div>
+                          <Link to="#" className="btn-main" onClick={addFollow}>
+                            Unfollow
+                          </Link>
+                        </>
+                      ) : (
+                        <>
+                          <div className="profile_follower">
+                            {author.followers} followers
+                          </div>
+                          <Link to="#" className="btn-main" onClick={addFollow}>
+                            Follow
+                          </Link>
+                        </>
+                      )}
                     </div>
                   </div>
                 </div>
-              </div>
+              </>
+            )}
+          </div>
 
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
+          <div className="col-md-12">
+            <div className="de_tab tab_simple">
+              <AuthorItems author={author} skelLoad={skelLoad} />
             </div>
           </div>
-        </section>
+        </div>
       </div>
     </div>
   );
-};
-
-export default Author;
+}
